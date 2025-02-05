ebd33fe07f37373c8059ee09bb12876ddb78bca9
hoed <hoedhud@gmail.com>
2025-02-05 09:08:50 +0700

    Update BookingSummaryCalculations.tsx

--- src/components/booking/BookingSummaryCalculations.tsx
+++ src/components/booking/BookingSummaryCalculations.tsx
@@ -68,10 +68,13 @@ export function BookingSummaryCalculations({
             <Calculator className="h-4 w-4 mr-2" />
             Calculate
           </Button>
-          <div className="flex justify-between items-center">
+      
+          {isStaff && (
+        <div className="flex justify-between items-center">
           <p className="font-semibold">Profit</p>
           <p className="font-semibold">{formatToIDR(calculatedTotal)}</p>
-        </div
+        </div>
+      )}
         </div>
       )}
 
