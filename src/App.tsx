05b2ea23a0c49fc2c9b3825bd836455b784310fe
hoed <hoedhud@gmail.com>
2025-02-07 13:50:35 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,17 @@ import { ThemeProvider } from "next-themes";
 import Index from "@/pages/Index";
 import Auth from "@/pages/Auth";
 import Dashboard from "@/pages/Dashboard";
-import ProfileSettings from "@/pages/ProfileSettings";
-import NavigationBar from "@/components/NavigationBar";
 import "./App.css";
 
 function App() {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Router>
-        <NavigationBar />
         <AuthProvider>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/profile-settings" element={<ProfileSettings />} />
           </Routes>
         </AuthProvider>
         <Toaster />
